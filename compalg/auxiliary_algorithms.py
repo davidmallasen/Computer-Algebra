e5e2b51d62e85b8872a47d371b77f9ea1d05d086
David Mallasen <dmallase@ucm.es>
2019-11-21 20:31:14 +0100

    Add poly_pth_root

--- compalg/auxiliary_algorithms.py
+++ compalg/auxiliary_algorithms.py
@@ -8,6 +8,7 @@ def repeated_square(a, n, mod=None):
     """
     Computes a^n % mod, with a belonging to a ring with 1 (not checked) and n > 0.
     """
+
     ns = list(bin(n)[2:])[1:]
 
     b = a
@@ -27,8 +28,30 @@ def horners_evaluation(f, u):
     """
     Fast evaluation of f(u).
     """
+
     if f.is_zero():
         return f.parent().base().zero()
 
     coefs = reversed(f.coefficients(sparse=False))
     return reduce(lambda acc, next: acc * u + next, coefs)
+
+
+def poly_pth_root(f):
+    """
+    Computes the pth root of the polynomial f in F_q[x] where q = p^r, p prime and f' = 0 (not checked)
+    There exists a polynomial g in F_q[x] such that f = g^p, i.e. a pth root of f, iff f' = 0.
+    """
+
+    field = f.parent()
+    p = field.base().characteristic()
+    q = field.base().order()
+
+    coefs = f.coefficients(sparse=False)
+    root_coefs = [0] * (1 + (len(coefs) / int(p)))
+
+    for i, coef in enumerate(reversed(coefs)):
+        if coef != 0:
+            j = len(coefs) - i - 1
+            root_coefs[j / p] = field(repeated_square(coef, q / p, q))  # a_j^{p^(r-1)} = a_j^{q/p}
+
+    return field(root_coefs)
