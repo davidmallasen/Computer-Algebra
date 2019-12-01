ec7815aa42dce856d1b914f60746e3d741f41a3a
Álvaro Rodríguez <alvarorgtr@gmail.com>
2019-12-01 17:20:32 +0100

    Buchberger algorithm & multivariate division implemented and working

---
+++ compalg/buchberger_algorithm.py
@@ -0,0 +1,122 @@
+"""
+Buchberger algorithm.
+"""
+from multivariate_division import multivariate_division_with_remainder
+from sage.all import *
+from sage.rings.polynomial.polynomial_ring import is_PolynomialRing
+from sage.rings.polynomial.multi_polynomial_ring import is_MPolynomialRing
+from sage.rings.ideal import is_Ideal
+import operator
+
+
+def __multidegree(f):
+    """
+    Computes the multidegree of f.
+    """
+    mon = f.monomials()
+    mon.sort()      # Sorts them with the ring order
+    return mon[-1].degrees()
+
+
+def __s_polynomial(g, h):
+    """
+    Computes the S-polynomial of g, h.
+    """
+    deg_g = __multidegree(g)
+    deg_h = __multidegree(h)
+    max_deg = map(max, zip(deg_g, deg_h))
+    R = g.parent()
+
+    # Builds a polynomial with the variables raised to max_deg, in order
+    vars = map(R, R.variable_names())
+    x_pow_max_deg = reduce(operator.mul, [x ** d for (d, x) in zip(max_deg, vars)], R(1))
+
+    quo_g, _ = x_pow_max_deg.quo_rem(g.lt())
+    quo_h, _ = x_pow_max_deg.quo_rem(h.lt())
+    return quo_g * g - quo_h * h
+
+
+def __unordered_pairs(l):
+    """
+    Generates a list of pairs from the given list, excluding reorders (if (a, b) is returned, (b, a) isn't), and
+    excluding pairs where the two elements are the same.
+
+    If two elements in the list are equal, this method will still consider them as different.
+    """
+    return [(l[i], l[j]) for i in range(len(l) - 1) for j in range(i + 1, len(l))]
+
+
+
+def buchberger_algorithm(I):
+    """
+    Buchberger algorithm for the computation of a Groebner basis.
+
+    Returns a list of polynomials that form a Groebner basis of the given ideal.
+
+    Parameters
+    ----------
+    I: an ideal of a multivariate polynomial ring over a field.
+
+    Returns
+    -------
+    The list of polynomials forming a Groebner basis.
+    """
+    if not is_Ideal(I):
+        raise TypeError('Argument should be an ideal')
+
+    poly_ring = I.ring()
+
+    if (not is_PolynomialRing(poly_ring)) and (not is_MPolynomialRing(poly_ring)):
+        raise TypeError('The ideal should be of a polynomial ring')
+
+    base_field = I.base_ring()
+
+    if not base_field.is_field():
+        raise TypeError('The ideal should be of a polynomial ring over a field')
+
+    # Initialize G with the generators of the ideal
+    G = list(I.basis)
+
+    # It will finish by construction of the algorithm
+    while True:
+        S = []
+        G.sort(reverse=True)    # Sorts with the polynomial ring ordering
+
+        for (g1, g2) in __unordered_pairs(G):
+            r = __s_polynomial(g1, g2)
+            _, r = multivariate_division_with_remainder(r, G)
+
+            if r != 0:
+                S.append(r)
+
+        # If S is empty, we already have a Groebner basis
+        if not S:
+            return G
+
+        G.extend(S)
+
+
+def __test_s_polynomial():
+    """
+    Pag. 606 modern computer algebra
+    """
+    poly_ring = PolynomialRing(QQ, 'x,y', order='deglex')
+    x, y = poly_ring('x'), poly_ring('y')
+    g = x ** 3 - 2 * x * y
+    h = x ** 2 * y - 2 * y ** 2 + x
+    print __s_polynomial(g, h)      # Expected -x^2
+
+
+def main():
+    """ Execute the examples. """
+    # Pag 609 modern computer algebra
+    poly_ring = PolynomialRing(QQ, 'x,y,z', order='deglex')     # You can specify, e.g., order='lex' to override <
+    x, y = poly_ring('x'), poly_ring('y')
+    f1 = x ** 3 - 2 * x * y
+    f2 = x ** 2 * y - 2 * y ** 2 + x
+    I = poly_ring.ideal(f1, f2)
+    print buchberger_algorithm(I)   # Expected f1, f2, -x^2, -2xy, -2y^2+x
+
+
+if __name__ == '__main__':
+    main()
---
+++ compalg/multivariate_division.py
@@ -0,0 +1,74 @@
+"""
+Multivariate division with remainder.
+"""
+from sage.all import *
+from sage.rings.polynomial.polynomial_ring import is_PolynomialRing
+from sage.rings.polynomial.multi_polynomial_ring import is_MPolynomialRing
+
+
+def multivariate_division_with_remainder(f, fs):
+    """
+    Performs multivariate division with remainder, that is, returns quotients q_1, ..., q_s and a remainder r such that
+    q_1 * f_1 + ... + q_s * f_s + r = f, and no monomial in r is divisible by any of lt(f_i).
+
+    Parameters
+    ----------
+    f : the numerator.
+    fs : the list of denominators.
+    Both f and all fs must belong to the same multivariate polynomial ring.
+
+    Returns
+    -------
+    qs: the quotients.
+    r: the remainder, f rem (f1, ..., fs).
+    """
+    poly_ring = f.parent()
+
+    if (not is_PolynomialRing(poly_ring)) and (not is_MPolynomialRing(poly_ring)):
+        raise TypeError('f and the fs should belong to a polynomial ring')
+
+    base_field = poly_ring.base_ring()
+
+    if not base_field.is_field():
+        raise TypeError('f and the fs should belong to a polynomial ring over a field')
+
+    if not all([poly_ring == g.parent()] for g in fs):
+        raise ValueError("All polynomials must belong to the same ring")
+
+    r = poly_ring(0)
+    p = f
+    q = [poly_ring(0)] * len(fs)
+
+    while not p.is_zero():
+        any_divides = False
+
+        for i in range(len(fs)):
+            fi = fs[i]
+
+            if fi.lt().divides(p.lt()):
+                div, _ = p.lt().quo_rem(fi.lt())
+                q[i] += div
+                p -= div * fi
+
+                any_divides = True
+                break
+
+        if not any_divides:
+            r += p.lt()
+            p -= p.lt()
+
+    return q, r
+
+
+def main():
+    """ Execute the examples. """
+    poly_ring = PolynomialRing(QQ, 'x,y', order='lex')
+    x, y = poly_ring('x'), poly_ring('y')
+    f = x * y ** 2 - x
+    f1 = x * y + 1
+    f2 = y ** 2 - 1
+    print multivariate_division_with_remainder(f, [f1, f2])     # Expected [y, 0], -x-y
+
+
+if __name__ == '__main__':
+    main()
